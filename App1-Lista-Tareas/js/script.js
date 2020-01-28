d7a5763c0621d1043b93f43d84e999a2ad4d0629
infzer0 <johnortizo@outlook.com>
2020-01-28 07:11:23 -0500

    JavaScript - Curso - Completar Constructor ListaTareas
    JavaScript - Curso: 14 Completar el Método Constructor de la Clase ListaTareas.

--- App1-Lista-Tareas/js/script.js
+++ App1-Lista-Tareas/js/script.js
@@ -2,6 +2,15 @@ class ListaTareas{
     constructor(){
         this.tareas = JSON.parse(localStorage.getItem('tareas'));
 
-        
+        if (!this.tareas) {
+            this.tareas = [
+                {tarea: 'Aprender JavaScript', completado: false},
+                {tarea: 'Aprender Python', completado: false},
+                {tarea: 'Aprender C++', completado: true}
+            ];
+        }
+
+        this.cargarTareas();
+        this.agregarEventListeners();
     }
 }
