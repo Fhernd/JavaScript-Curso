5dd0d62a29ed4339a17fdcfae9581c22a8e8849c
infzer0 <johnortizo@outlook.com>
2020-02-02 12:52:20 -0500

    JavaScript - Curso - Crear Clase CreadorMemes
    JavaScript - Curso: 40 Inicio Codificación de la Clase Personalizada CreadorMemes.

---
+++ App2-Creador-Memes/js/script.js
@@ -0,0 +1,13 @@
+const ANCHO_DISPOSITIVO = window.innerWidth;
+
+class CreadorMemes{
+    constructor() {
+        this.imagenCanvas = document.getElementById('imagenCanvas');
+        this.imagen = document.getElementById('imagen');
+        this.textoSuperior = document.getElementById('textoSuperior');
+        this.textoInferior = document.getElementById('textoInferior');
+        this.descargarMeme = document.getElementById('descargarMeme');
+
+        
+    }
+}
