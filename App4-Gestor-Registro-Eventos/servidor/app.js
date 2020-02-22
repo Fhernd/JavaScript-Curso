e132107289f44d6f6f8b018765ffd74251e3439f
infzer0 <johnortizo@outlook.com>
2020-02-22 12:35:47 -0500

    JavaScript - Curso - Especificación Middleware body-parser
    JavaScript - Curso 66: Configuración de body-parser para Leer el Contenido body de la Solicitud.

--- App4-Gestor-Registro-Eventos/servidor/app.js
+++ App4-Gestor-Registro-Eventos/servidor/app.js
@@ -4,6 +4,17 @@ const http = require('http');
 
 let app = express();
 app.set('port', 3900);
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.send('Bienvenido a la API de Registro de Eventos.');
+});
 
 let servidor = http.Server(app);
 servidor.listen(app.get('port'), function() {
