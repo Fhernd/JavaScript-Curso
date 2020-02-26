b55ce7a1a1c2cf2952052e27eb562bcbec322afd
infzer0 <johnortizo@outlook.com>
2020-02-26 11:41:02 -0500

    JavaScript - Curso - Método generarEmojis()
    JavaScript - Curso: 110 Creación del Método de Instancia generarEmojis() de la Clase EmojiMatchingGame.

--- App5-Juego-Emojis/js/script.js
+++ App5-Juego-Emojis/js/script.js
@@ -13,7 +13,22 @@ class EmojiMatchingGame{
         for(let i = 1; i < this.cantidadEmojis; ++i){
             let nuevoEmoji = document.createElement('img');
             nuevoEmoji.src = this.imagenEmoji;
-            
+            let x = Math.floor(Math.random() * 400);
+            let y = Math.floor(Math.random() * 420);
+            nuevoEmoji.style.left = `${x}px`;
+            nuevoEmoji.style.top = `${y}px`;
+
+            this.panelIzquierdo.appendChild(nuevoEmoji);
         }
+
+        this.configurarPaneles();
+    }
+
+    configurarPaneles() {
+        let clonacionEmojis = this.panelIzquierdo.cloneNode(true);
+        clonacionEmojis.removeChild(clonacionEmojis.lastChild);
+
+        this.panelDerecho.appendChild(clonacionEmojis);
+        
     }
 }
